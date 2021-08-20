b80e92e6534600f31c295ad1d9b9d78569d98a0f
KozakoPolako <dariusz.dabrowski1998@gmail.com>
2021-08-20 14:25:51 +0200

    add server files

---
+++ server/app.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+
+const app = express();
+
+const corsOptions = {
+    origin: 'https://localhost:3000'
+}
+
+app.use(cors());
+
+app.use(express.static('games'));
+
+app.get("/list", (req,res) => {
+
+    fs.readdir("./games", (err,files) => {
+        res.json(files);
+    });
+    //res.send("Print list on the server");
+
+})
+
+
+
+const server = app.listen(8081, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log(`Express server running at ${host} : ${port}`);
+});
